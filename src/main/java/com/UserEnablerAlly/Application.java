7a934cb20a30c742ae22478b580153a23d23e929
Christian-Ro <chrisstian91@gmail.com>
2020-12-15 21:18:07 -0600

    initial commit

---
+++ src/main/java/com/UserEnablerAlly/Application.java
@@ -0,0 +1,13 @@
+package com.UserEnablerAlly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        //Simple crud application using spring boot
+        SpringApplication.run(Application.class, args);
+    }
+
+}
